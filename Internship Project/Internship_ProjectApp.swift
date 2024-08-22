3f2d66d0817fbae88789e7d76d0c72e485ac01e6
Zeynep Baran <120269144+baranzeyn@users.noreply.github.com>
2024-08-22 20:42:04 +0300

    Initial Commit

---
+++ Internship Project/Internship_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Internship_ProjectApp.swift
+//  Internship Project
+//
+//  Created by Zeynep Baran on 22.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Internship_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
